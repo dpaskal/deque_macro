5f5e4ee745be51be263e05c892853877e7631c9c
Daniel Paskalev <dpaskal1@binghamton.edu>
2020-09-14 21:09:59 -0400

    new level

--- Makefile
+++ Makefile
@@ -1,11 +1,16 @@
-FLAGS = -std=c++11 -Wall -Wextra -g -pedantic
+#FLAGS = -std=c++11 -Wall -Wextra -g -pedantic
+FLAGS = -std=c++1z -Wall -Wextra -pedantic -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wnull-dereference -Wrestrict -Wuseless-cast -Wformat=2
 
-all: deq
 
-deq: deque.h deque.cpp
+all: deq tester
+
+deq: Deque.hpp deque.cpp
 	g++ deque.cpp -o deq $(FLAGS)
+
+tester: Deque.hpp test.cpp
+	g++ test.cpp -o tester $(FLAGS) -ldl
 
 .phony:clean
 
 clean:
-	rm -f deq
+	rm -f deq tester
--- deque.cpp
+++ deque.cpp
@@ -1,9 +1,22 @@
-#include "deque.h"
+#include "Deque.hpp"
 #include <iostream>
 
+Deque_DEFINE(int)
 
 int main() {
-	deque_define(int);
+	Deque_int deq;
+	// deq.push_front(5);
+
 	std::cout << "definition of deque" << std::endl;
+	std::cout << sizeof(long) << std::endl;
+	std::cout << INT32_MAX << std::endl;
+	std::cout << INT64_MAX << std::endl;
+	
+	std::cout << "Size of char: " << sizeof(char) << " byte" << std::endl;
+    std::cout << "Size of int: " << sizeof(int) << " bytes" << std::endl;
+    std::cout << "Size of long: " << sizeof(long) << " bytes" << std::endl;
+
+    std::cout << "Size of float: " << sizeof(float) << " bytes" << std::endl;
+    std::cout << "Size of double: " << sizeof(double) << " bytes" << std::endl;
 	return 0;
 }
